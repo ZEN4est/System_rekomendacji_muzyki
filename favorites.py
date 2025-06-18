428c899540dfa6e69f9f441b6e8a46ddfe7dc1c9
miwn@mwdesktop <wnekmiki@gmail.com>
2025-06-18 10:32:01 +0200

    checkpoint 2

--- favorites.py
+++ favorites.py
@@ -1,12 +1,82 @@
 from cProfile import label
 import tkinter as tk
 from tkinter import ttk
-from model.crud import get_favorites_by_user_id, get_songs_in_playlist, remove_song_from_playlist
+from turtle import bgcolor
+from model.crud import add_song_to_playlist, get_favorites_by_user_id, get_playlist_song, get_songs_in_playlist, remove_song_from_playlist, \
+                        get_playlists_by_user
 from model.models import Playlist, Song, User
 from sqlalchemy.orm import Session
 
+class ScrollablePopup:
+    def __init__(self, session: Session, parent, x, y, content_data, song: Song, style_dict):
+        self.session = session
+        self.style_dict=style_dict
+        self.top = tk.Toplevel(parent)
+        self.top.title('Playlists')
+        self.top.geometry(f'+{x}+{y}')
+        self.top.transient(parent)
+        self.top.wait_visibility()
+        self.top.grab_set()
+        self.top.focus_set()
+        self.song = song
+        self.content_data = content_data
+
+        self.create_scrollable_frame(content_data)
+
+        self.top.protocol("WM_DELETE_WINDOW", self.on_close_popup)
+
+    def create_scrollable_frame(self, content_labels_data):
+        self.canvas = tk.Canvas(self.top, borderwidth=0, background=self.style_dict['bg'])
+        self.scrollbar = ttk.Scrollbar(self.top, orient='vertical', command=self.canvas.yview)
+        self.canvas.configure(yscrollcommand=self.scrollbar.set)
+
+        self.scrollbar.pack(side='right', fill='y')
+        self.canvas.pack(side='left', fill='both', expand=True)
+
+        s = ttk.Style()
+        s.configure('TFrame', background=self.style_dict['bg'])
+        self.scrollable_frame = ttk.Frame(self.canvas, style='TFrame')
+        self.canvas.create_window((0, 0), window=self.scrollable_frame, anchor='nw')
+
+        self.scrollable_frame.bind(
+            '<Configure>',
+            lambda e: self.canvas.configure(
+                scrollregion=self.canvas.bbox('all')
+            )
+        )
+
+        if not content_labels_data.keys():
+            empty_label = tk.Label(self.scrollable_frame, text='No playlists found',
+                                   font=self.style_dict['empty_font'], fg=self.style_dict['song_color'], bg=self.style_dict['bg'])
+            empty_label.pack(pady=(20, 0), padx=(20, 0), side=tk.TOP, anchor='center')
+            self.scrollable_frame.bind('<Button-1>', lambda e: self.on_close_popup)
+        for playlist, has in content_labels_data.items():
+            label = ttk.Label(self.scrollable_frame, text=playlist.name, borderwidth=1, relief='solid', padding=(5, 2))
+            label.configure(background=self.style_dict['bg'])
+            if has:
+                label.configure(foreground=self.style_dict['song_color'])
+            else:
+                label.configure(foreground='grey')
+            label.pack(fill='x', padx=5, pady=2)
+            label.bind('<Button-1>', lambda e: self.on_clickable_label_click(e, playlist, has))
+            label.config(cursor='hand2')
+
+    def on_clickable_label_click(self, event, playlist, has):
+        if has:
+            remove_song_from_playlist(self.session, playlist_id=playlist.playlist_id, song_id=self.song.song_id)
+            print(f'Removed {self.song.title} from {playlist.name}')
+        else:
+            add_song_to_playlist(self.session, playlist_id=playlist.playlist_id, song_id=self.song.song_id)
+            print(f'Added {self.song.title} to {playlist.name}')
+
+        self.on_close_popup()
+
+    def on_close_popup(self):
+        self.top.grab_release()
+        self.top.destroy()
+
 def get_song_in_favorites_frame(outer_frame: tk.Frame, session: Session, idx: int, h: int,
-                                song: Song, favorites_id: int) -> tk.Frame:
+                                song: Song, favorites_id: int, playlist_has_song: dict[Playlist, bool]) -> tk.Frame:
     bg = '#003d2f'
     song_color = '#3CE7A7'
     artist_color = '#2AB481'
@@ -19,6 +89,7 @@ def get_song_in_favorites_frame(outer_frame: tk.Frame, session: Session, idx: in
     outer_width = outer_frame.winfo_width()
     pad_small=outer_width // 100
     entry_font = ('Segoe UI', outer_width // 50)
+    print(entry_font)
     song_icon_font = ('Segoe UI', outer_width // 15, 'bold')
 
     frame = tk.Frame(outer_frame, bg=bg, height=h, width=outer_width)
@@ -41,9 +112,22 @@ def get_song_in_favorites_frame(outer_frame: tk.Frame, session: Session, idx: in
     artist_label = tk.Label(song_info_frame, bg=bg, fg=artist_color, text=artist_text, font=entry_font)
     artist_label.pack(side='top', anchor='w')
 
-    # --- Button ----
-    remove_label = tk.Label(frame, bg=bg, fg=icon_dark, text = '❌', font=song_icon_font)
-    remove_label.grid(row=0, column=2, sticky='nse')
+    # --- Buttons ----
+    button_frame= tk.Frame(frame, bg=bg)
+    button_frame.grid(row=0, column=2, sticky='nse')
+
+    add_label = tk.Label(button_frame, bg=bg, fg=icon_dark, text='➕', font=song_icon_font)
+
+    def add(_event):
+        label_x = add_label.winfo_rootx()
+        label_y = add_label.winfo_rooty()
+        label_width = add_label.winfo_width()
+        popup_x = label_x + label_width + pad_small * 2
+        popup_y = label_y
+        ScrollablePopup(session, add_label.winfo_toplevel(), popup_x, popup_y, playlist_has_song, song,
+                        style_dict={'bg': bg, 'empty_font': song_icon_font, 'song_color': song_color})
+
+    remove_label = tk.Label(button_frame, bg=bg, fg=icon_dark, text = '❌', font=song_icon_font)
 
     def remove(_event):
         remove_song_from_playlist(session, favorites_id, song.song_id)
@@ -55,9 +139,16 @@ def get_song_in_favorites_frame(outer_frame: tk.Frame, session: Session, idx: in
     def on_leave(e):
         e.widget['foreground'] = icon_dark
 
+    add_label.bind('<Button-1>', add)
+    add_label.bind('<Enter>', on_enter)
+    add_label.bind('<Leave>', on_leave)
+
     remove_label.bind('<Button-1>', remove)
     remove_label.bind('<Enter>', on_enter)
     remove_label.bind('<Leave>', on_leave)
+
+    remove_label.pack(side=tk.RIGHT, padx=(0, pad_small))
+    add_label.pack(side=tk.RIGHT)
 
     return frame
 
@@ -90,21 +181,26 @@ def show_favorites_screen(content_frame: tk.Frame, session: Session, user: User)
     heart_icon = tk.Label(label_frame, text='♥', font=icon_big_font, bg=bg, fg=icon_dark)
     heart_icon.pack(pady=(pad_big, pad_small), side=tk.LEFT)
 
-    heart_text = tk.Label(label_frame, text='Favorites', font=label_big_font, fg=icon_bright, bg=bg)
+    heart_text = tk.Label(label_frame, text='Favorites', font=label_big_font, fg='#3fbf7f', bg=bg)
     heart_text.pack(side=tk.LEFT, pady=(pad_big, pad_small))
 
     favorites_list_frame = tk.Frame(content_frame, bg=bg, width=width, height=height)
     favorites_list_frame.pack()
     favorites_list_frame.pack_propagate(False)
 
+    plalists_id_to_haas_song = {}
+    playlists_all = get_playlists_by_user(session, user.user_id)
+
+
     songs = get_songs_in_playlist(session, favorites.playlist_id)
     if len(songs) == 0:
         empty_label = tk.Label(favorites_list_frame, text='Empty...', font=empty_font, fg=song_color, bg=bg)
         empty_label.pack(pady=(pad_big, 0), side=tk.TOP)
     for idx, song in enumerate(songs):
+        playlist_has_song: dict[Playlist, bool] = {p: bool(get_playlist_song(session, playlist_id=p.playlist_id, song_id=song.song_id)) for p in playlists_all if p.name != 'favorites'}
         track_frame = get_song_in_favorites_frame(
             outer_frame=favorites_list_frame, session=session, idx=idx, h=list_height,
-            song=song, favorites_id=favorites.playlist_id
+            song=song, favorites_id=favorites.playlist_id, playlist_has_song=playlist_has_song
         )
         track_frame.pack_propagate(False)
         track_frame.pack(side=tk.TOP, pady=(0, pad_small // 2), fill='x')
